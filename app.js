3e562bcc5e0b719c2fa74f4e01dcf4e97c6f6325
Subodh <subodh.dhakal@mavs.uta.edu>
2019-05-27 19:57:57 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = r
+equire("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use("view engine", "ejs");
+
+app.get("/", function(req, res) {   //When user tries to access the home route, res.send("Hello")
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    
+    switch(currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error Occured. Current Day unavailable.");
+    }
+    res.render("list", {kindOfday: day});
+});
+
+app.listen(3000, function() {
+    console.log("Server started at port 3000");
+});
