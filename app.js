ab39b159acc73358fb4686106a3201775c649c46
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-06-16 23:47:37 -0500

    gitignore added...Deploy Finalized

--- app.js
+++ app.js
@@ -126,6 +126,11 @@ app.get("/about", function(req, res) {
     res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if(port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function() {
     console.log("Server started at port 3000");
 });
