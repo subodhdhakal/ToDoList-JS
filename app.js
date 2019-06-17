69a6505c2c41cbec31d7d38e9768a00020f22f7e
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-06-16 23:35:40 -0500

    Mongo Atlas DB connected

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-subodh:2580@subodh-m0uhl.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
     name: String
